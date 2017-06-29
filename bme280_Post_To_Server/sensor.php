9cd55b9087316532c24f899e64d84779d3b86d25
sosteen <sosteen@gmail.com>
2017-06-28 23:17:02 -0700

    server receiver script

---
+++ bme280_Post_To_Server/sensor.php
@@ -0,0 +1,27 @@
+<?php
+define('LBASE_DIR',dirname(__FILE__));
+define('CONFIGFILE', 'sensor1.html');
+
+error_reporting(E_ALL);
+
+$temp = $_POST['temp'];
+$hum  = $_POST['hum'];
+$pres = $_POST['pres'];
+$strgth = $_POST['strength'];
+$alt = $_POST['alt'];
+$dew = $_POST['dew'];
+
+$fp = fopen(LBASE_DIR . '/' . CONFIGFILE, 'w');
+
+fwrite($fp, "<html><title>Sensor Readings</title><body><ul><li>Temperature: ".$temp."&#176;F</li>\n");
+fwrite($fp, "<li>Humidity: ".$hum."% rh</li>\n");
+fwrite($fp, "<li>Pressure: ".$pres." atm</li>\n");
+fwrite($fp, "<li>Altitude: ".$alt." ft</li>\n");
+fwrite($fp, "<li>Dew Point: ".$dew."&#176;F</li>\n");
+fwrite($fp, "<li>Signal Strength: ".$strgth." db</li>\n");
+fwrite($fp, '</ul></body></html>');
+
+fclose($fp);
+exit();
+
+?>
